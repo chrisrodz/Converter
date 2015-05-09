aa24c4082c7154853b580d855f58e5c3b13edaa3
Christian <christian.etpr10@gmail.com>
2015-05-09 08:55:58 -0400

    App Version 1.0

--- Converter/ConverterViewController.swift
+++ Converter/ConverterViewController.swift
@@ -7,6 +7,26 @@
 //
 
 import UIKit
+import Darwin
+
+// Code from: http://stackoverflow.com/questions/26790660/how-to-convert-a-binary-to-decimal-in-swift
+extension String {
+    var hexToInt      : Int    { return Int(strtoul(self, nil, 16))    }
+    var hexToDouble   : Double { return Double(strtoul(self, nil, 16)) }
+    var hexToBinary   : String { return String(hexToInt, radix: 2)    }
+    var intToHex      : String { return String(toInt()!, radix: 16)    }
+    var intToBinary    : String { return String(toInt()!, radix: 2)     }
+    var binaryToInt    : Int    { return Int(strtoul(self, nil, 2))     }
+    var binaryToDouble : Double { return Double(strtoul(self, nil, 2))  }
+    var binaryToHex   : String { return String(binaryToInt, radix: 16) }
+}
+
+extension Int {
+    var binaryString: String { return String(self, radix: 2)  }
+    var hexString  : String { return String(self, radix: 16) }
+    var doubleValue : Double { return Double(self) }
+}
+
 
 class ConverterViewController: UIViewController, UIPickerViewDelegate, UITextFieldDelegate {
 
@@ -14,19 +34,32 @@ class ConverterViewController: UIViewController, UIPickerViewDelegate, UITextFie
     @IBOutlet weak var toBaseField: UITextField!
     @IBOutlet weak var numberTextfield: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
-    @IBOutlet weak var basePicker: UIPickerView!
     @IBOutlet weak var creditLabel: UILabel!
-    @IBOutlet weak var dimView: UIView!
-
     
-    var selected = "from"
+    let bases = ["Decimal", "Binary", "Hexadecimal"]
     
-    let bases = ["Decimal", "Binary", "Octal", "Hexadecimal"]
+    let binaryCharacters : [Character] = ["0", "1"]
+    let decimalCharacters : [Character] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    let hexadecimalCharacters : [Character] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let basePicker = UIPickerView(frame: CGRectMake(0, 200, view.frame.width, 300))
+        basePicker.showsSelectionIndicator = true
+        let toolbar = UIToolbar()
+        toolbar.barStyle = UIBarStyle.Default
+        toolbar.translucent = true
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: "donePicker")
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: nil, action: nil)
+        toolbar.setItems([spaceButton, doneButton], animated: false)
         
-        basePicker.hidden = true
+        fromBaseField.inputView = basePicker
+        fromBaseField.inputAccessoryView = toolbar
+        toBaseField.inputView = basePicker
+        toBaseField.inputAccessoryView = toolbar
+
         fromBaseField.text = bases[0]
         toBaseField.text = bases[1]
         
@@ -35,19 +68,46 @@ class ConverterViewController: UIViewController, UIPickerViewDelegate, UITextFie
         toBaseField.delegate = self
         numberTextfield.delegate = self
         
-        let singleTap = UITapGestureRecognizer(target: self, action: "dismissPicker")
-        self.dimView.addGestureRecognizer(singleTap)
+        fromBaseField.borderStyle = UITextBorderStyle.RoundedRect
+        toBaseField.borderStyle = UITextBorderStyle.RoundedRect
+        numberTextfield.borderStyle = UITextBorderStyle.RoundedRect
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        view.endEditing(true)
     }
 
     @IBAction func convertNumber(sender: AnyObject) {
+        let number: String! = numberTextfield.text
+        let toBase = toBaseField.text
+        let fromBase = fromBaseField.text
         
-    }
-    
-    func dismissPicker() {
-        basePicker.hidden = true
-        dimView.hidden = false
-        UIView.animateWithDuration(0.4) {
-            self.dimView.alpha = 0.0
+        if numberIsValidInBase(number.lowercaseString, base: fromBase) {
+            if fromBase == "Decimal" && toBase == "Binary" {
+                resultLabel.text = number.toInt()?.binaryString
+            } else if fromBase == "Decimal" && toBase == "Hexadecimal" {
+                resultLabel.text = number.toInt()?.hexString
+            } else if fromBase == "Binary" && toBase == "Hexadecimal" {
+                resultLabel.text = number.binaryToHex
+            } else if fromBase == "Binary" && toBase == "Decimal" {
+                resultLabel.text = String(number.binaryToInt)
+            } else if fromBase == "Hexadecimal" && toBase == "Decimal" {
+                resultLabel.text = String(number.hexToInt)
+            } else if fromBase == "Hexadecimal" && toBase == "Binary" {
+                resultLabel.text = number.hexToBinary
+            } else if toBase == fromBase {
+                let alert = UIAlertController(title: "Same bases", message: "Choose different bases", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                presentViewController(alert, animated: false, completion: nil)
+            } else {
+                let alert = UIAlertController(title: "Unknown error", message: "Something weird happened. Sorry.", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+                presentViewController(alert, animated: false, completion: nil)
+            }
+        } else {
+            let alert = UIAlertController(title: "Error", message: number + " is not in base " + fromBase, preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            presentViewController(alert, animated: false, completion: nil)
         }
     }
 
@@ -67,46 +127,42 @@ class ConverterViewController: UIViewController, UIPickerViewDelegate, UITextFie
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
     {
-        if selected == "from" {
-            fromBaseField.text = bases[row]
-        }
-        if selected == "to" {
-            toBaseField.text = bases[row]
-        }
-        selected = ""
-        convertNumber(self)
-        basePicker.hidden = true
+        var textFields = [fromBaseField, toBaseField]
         
-        dimView.hidden = false
-        UIView.animateWithDuration(0.4) {
-            self.dimView.alpha = 0.0
+        for field in textFields {
+            if field.isFirstResponder() {
+                field.text = bases[row]
+                break;
+            }
         }
     }
     
-    // MARK: UITextFieldDelegate
-    
-    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
-        if textField.tag == 1 {
-            selected = "from"
-        }
-        if textField.tag == 2 {
-            selected = "to"
-        }
-        if textField.tag == 3 {
-            selected = ""
-            return true
-        }
-        creditLabel.hidden = true
-        
-        dimView.hidden = false
-        UIView.animateWithDuration(0.4) {
-            self.dimView.alpha = 0.6
-        }
-        
-        basePicker.hidden = false
-        return false
+    func donePicker() {
+        view.endEditing(true)
     }
     
+    func numberIsValidInBase(number: String, base: String) -> Bool {
+        if base == "Decimal" {
+            for character in number {
+                if contains(decimalCharacters, character) == false {
+                    return false
+                }
+            }
+        } else if base == "Binary" {
+            for character in number {
+                if contains(binaryCharacters, character) == false {
+                    return false
+                }
+            }
+        } else if base == "Hexadecimal" {
+            for character in number {
+                if contains(hexadecimalCharacters, character) == false {
+                    return false
+                }
+            }
+        }
+        return true
+    }
 
 }
 
